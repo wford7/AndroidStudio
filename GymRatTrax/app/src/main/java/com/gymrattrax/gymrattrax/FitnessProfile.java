1700ebd62163b73bc2202cd09c8268a0ae4f71ef
wford7 <WFORD@Wesleys-MacBook-Pro-3.local>
2015-03-05 05:59:16 -0500

    Added UI functionality

--- GymRatTrax/app/src/main/java/com/gymrattrax/gymrattrax/FitnessProfile.java
+++ GymRatTrax/app/src/main/java/com/gymrattrax/gymrattrax/FitnessProfile.java
@@ -14,20 +14,52 @@ public class FitnessProfile extends ActionBarActivity {
 
     private Button SaveProfileButton;
     private Button EditProfileButton;
-    private EditText NameEditText;
-    private EditText BirthDateEditText;
+    private EditText nameEditText;
+    private EditText birthDateEditText;
+    private EditText weightEditText;
+    private EditText heightEditText;
+    private EditText fatPercentageEditText;
     //private Spinner
 
-    //database query and then set editTexts to display the appropriate data
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness_profile);
 
+        //database query and then set editTexts to display the appropriate data
+
         SaveProfileButton = (Button)findViewById(R.id.SaveProfileButton);
         EditProfileButton = (Button)findViewById(R.id.EditProfileButton);
 
-        NameEditText = (EditText)findViewById(R.id.NameEditText);
+        nameEditText = (EditText)findViewById(R.id.profile_name);
+        birthDateEditText = (EditText)findViewById(R.id.birth_date);
+        weightEditText = (EditText)findViewById(R.id.profile_weight);
+        heightEditText = (EditText)findViewById(R.id.profile_height);
+        fatPercentageEditText = (EditText)findViewById(R.id.fat_percentage);
+
+        // make edit text unclickable until edit button is clicked
+        nameEditText.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        nameEditText.setEnabled(false);
+        nameEditText.setClickable(false);
+
+        birthDateEditText.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        birthDateEditText.setEnabled(false);
+        birthDateEditText.setClickable(false);
+
+        weightEditText.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        weightEditText.setEnabled(false);
+        weightEditText.setClickable(false);
+
+        heightEditText.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        heightEditText.setEnabled(false);
+        heightEditText.setClickable(false);
+
+        fatPercentageEditText.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        fatPercentageEditText.setEnabled(false);
+        fatPercentageEditText.setClickable(false);
+
+
+
 
         SaveProfileButton.setOnClickListener(new Button.OnClickListener(){
 
@@ -41,7 +73,25 @@ public class FitnessProfile extends ActionBarActivity {
 
             @Override
             public void onClick(View view) {
-                editProfile(view);
+                nameEditText.setBackgroundColor(getResources().getColor(android.R.color.white));
+                nameEditText.setEnabled(true);
+                nameEditText.setClickable(true);
+
+                birthDateEditText.setBackgroundColor(getResources().getColor(android.R.color.white));
+                birthDateEditText.setEnabled(true);
+                birthDateEditText.setClickable(true);
+
+                weightEditText.setBackgroundColor(getResources().getColor(android.R.color.white));
+                weightEditText.setEnabled(true);
+                weightEditText.setClickable(true);
+
+                heightEditText.setBackgroundColor(getResources().getColor(android.R.color.white));
+                heightEditText.setEnabled(true);
+                heightEditText.setClickable(true);
+
+                fatPercentageEditText.setBackgroundColor(getResources().getColor(android.R.color.white));
+                fatPercentageEditText.setEnabled(true);
+                fatPercentageEditText.setClickable(true);
             }
         });
 
--- GymRatTrax/app/src/main/java/com/gymrattrax/gymrattrax/HomeScreen.java
+++ GymRatTrax/app/src/main/java/com/gymrattrax/gymrattrax/HomeScreen.java
@@ -1,27 +1,41 @@
 package com.gymrattrax.gymrattrax;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 // import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ScrollView;
+import android.widget.ImageView;
 import android.widget.Button;
 import android.content.Intent;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 
 
 public class HomeScreen extends ActionBarActivity {
 
     private Button BeginWorkoutButton, ViewScheduleButton, ViewProfileButton, ViewProgressButton, CalorieNegationButton, EditSettingsButton;
+    private ImageView GymRat;
+    private ScrollView updateLayoutScroll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Context mContext = this;
         super.onCreate(savedInstanceState);
         //initiate tutorial/profile creation if there is no Profile ID in database
         Profile create = new Profile();
         setContentView(R.layout.activity_home_screen);
+        final Animation animTranslate = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
 
 
+        GymRat = (ImageView)findViewById(R.id.home_rat);
         BeginWorkoutButton = (Button)findViewById(R.id.BeginWorkoutButton);
         ViewScheduleButton = (Button)findViewById(R.id.ViewScheduleButton);
         ViewProfileButton = (Button)findViewById(R.id.ViewProfileButton);
@@ -29,11 +43,28 @@ public class HomeScreen extends ActionBarActivity {
         CalorieNegationButton = (Button)findViewById(R.id.CalorieNegationButton);
         EditSettingsButton = (Button)findViewById(R.id.EditSettingsButton);
 
+
+        GymRat.setOnClickListener(new ImageView.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animTranslate);
+            }});
+
         BeginWorkoutButton.setOnClickListener(new Button.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                loadCurrentSchedule(view);
+                //loadCurrentSchedule(view);
+                LinearLayout newTest = (LinearLayout)findViewById(R.id.LayoutScroll);
+                TextView newText = new TextView(mContext);
+                newText.setText("Dynamic Add Text Test");
+                updateLayoutScroll = (ScrollView)findViewById(R.id.scrollView);
+
+                newTest.addView(newText);
+
+                //updateLayoutScroll.addView(newTest);
+
             }
         });
 
