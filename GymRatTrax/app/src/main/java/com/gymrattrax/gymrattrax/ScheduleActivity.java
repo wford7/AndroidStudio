67c15733b4b145e5bfaa1a409f103417f929c914
Aly Wright <awrigh36@students.kennesaw.edu>
2015-03-20 18:21:34 -0400

    Schedule Activity
    Streamlining for the add workout ui. Type selection is now a popup
    instead of its own screen.

--- GymRatTrax/app/src/main/java/com/gymrattrax/gymrattrax/ScheduleActivity.java
+++ GymRatTrax/app/src/main/java/com/gymrattrax/gymrattrax/ScheduleActivity.java
@@ -8,24 +8,24 @@ import android.provider.CalendarContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ActionMenuView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
+import static com.gymrattrax.gymrattrax.R.id.schedule_google_calendar_button;
 
 
 public class ScheduleActivity extends ActionBarActivity {
-    private Button backScheduleButton;
-    private Button addWorkoutButton;
-    private Button editWorkoutButton;
-    private Button openGoogleCalendarButton;
 
     GridView gridView;
 
@@ -60,16 +60,15 @@ public class ScheduleActivity extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 if (position == 0 || position == 3 || position == 6 || position == 9) {
-                    String workoutId = "Strength#1";
                     Toast.makeText(getApplicationContext(), ((TextView) view).getText() + ": WORKOUT DETAILS", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
-        addWorkoutButton = (Button) findViewById(R.id.addWorkoutButton);
-        editWorkoutButton = (Button) findViewById(R.id.schedule_activity_edit_workout);
-        backScheduleButton = (Button) findViewById(R.id.schedule_back_button);
-        openGoogleCalendarButton = (Button) findViewById(R.id.schedule_google_calendar_button);
+        final Button addWorkoutButton = (Button) findViewById(R.id.addWorkoutButton);
+        Button editWorkoutButton = (Button) findViewById(R.id.schedule_activity_edit_workout);
+        Button backScheduleButton = (Button) findViewById(R.id.schedule_back_button);
+        final Button openGoogleCalendarButton = (Button) findViewById(schedule_google_calendar_button);
 
         backScheduleButton.setOnClickListener(new Button.OnClickListener() {
 
@@ -82,21 +81,45 @@ public class ScheduleActivity extends ActionBarActivity {
         addWorkoutButton.setOnClickListener(new Button.OnClickListener() {
 
             @Override
-            public void onClick(View view) {
-                loadAddWorkout(view);
-            }
-        });
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(ScheduleActivity.this, v);
+                popup.inflate(R.menu.menu_add_workout);
+                popup.show();
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.add_workout_strength:
+                                ScheduleActivity.this.loadStrengthWorkout();
+                                return true;
+                            case R.id.add_workout_cardio:
+                                ScheduleActivity.this.loadCardioWorkout();
+                                return true;
+                            default:
+                                return false;
+                        }
+                    }
+                });
 
-//        openGoogleCalendarButton.setOnClickListener(new Button.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View view) {
-//                Calendar dateToShow = Calendar.getInstance();
-//                dateToShow.set(2015, Calendar.MARCH, 25, 17, 0);
-//                loadViewSchedule(dateToShow);
-//            }
-//        });
+
+            }
+
+            ;
+
+        });
     }
+
+//                openGoogleCalendarButton.setOnClickListener(new Button.OnClickListener() {
+//
+//                    @Override
+//                    public void onClick(View view) {
+//                        Calendar dateToShow = Calendar.getInstance();
+//                        dateToShow.set(2015, Calendar.MARCH, 25, 17, 0);
+//                        loadViewSchedule(dateToShow);
+//                    }
+//                });
+//            }
+
 
 //        editWorkoutButton.setOnClickListener(new Button.OnClickListener() {
 //
@@ -107,51 +130,16 @@ public class ScheduleActivity extends ActionBarActivity {
 //        });
 
 
-    private void loadAddWorkout(View view) {
-        Intent intent = new Intent(ScheduleActivity.this, AddWorkoutActivity.class);
+    private void loadStrengthWorkout() {
+        Intent intent = new Intent(ScheduleActivity.this, StrengthWorkoutActivity.class);
         startActivity(intent);
     }
 
-//CALENDAR SYNCING
-// MOVE THIS TO CALENDAR SERVICE
-//    private void addWorkout(String exercise1, Calendar begin, Calendar end) {
-//        Calendar dateToShow = Calendar.getInstance();
-//        dateToShow.set(2015, Calendar.MARCH, 12, 17, 00);
-//        long epochMillis = dateToShow.getTimeInMillis();
-//
-//        String data = "" + begin.toString() + "" + end.toString();
-//        Uri eventUri = CalendarService.addEvent(ctx, accountName, "EXERCISE 1", data);
-//        Uri.Builder uriBuilder = eventUri.buildUpon();
-//        uriBuilder.appendPath("time");
-//        ContentUris.appendId(uriBuilder, epochMillis);
-//        Uri uri = uriBuilder.build();
-//
-//        Intent intent = new Intent(Intent.ACTION_VIEW);
-//        intent.setData(uri);
-//        startActivity(intent);
-//    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
+    private void loadCardioWorkout() {
+        Intent intent = new Intent(ScheduleActivity.this, CardioWorkoutActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void loadViewSchedule(Calendar dateToShow) {
         long epochMillis = dateToShow.getTimeInMillis();
@@ -166,3 +154,4 @@ public class ScheduleActivity extends ActionBarActivity {
         startActivity(intent);
     }
 }
+
